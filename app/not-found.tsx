ee4083bbe82544ced5b0c3909478e50e46451f7f
Jason O'Dell <jason@vitalflohealth.com>
2023-04-10 12:06:58 -0400

    Updating 404 to not be like VFs

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,21 @@
-'use client'
+import Link from 'next/link'
 
-export default function NotFound() {
+export default function NotFoundPage() {
   return (
-    <div>
-      <div>
-        <h2>Error 404</h2>
-      </div>
-      <div>
-        <p>
-          Well this is genuinely embarrassing for us. We are not able to locate
-          the page you are looking for, but don`&apos;`t worry. We have a few
-          steps you can take to get back on track.{' '}
-        </p>
-        <p>1.Refresh the page.</p>
-        <p>2.Go back to the last page and try to click through again.</p>
-        <p>
-          If you tried both of those to no avail, please contact our product
-          team at{' '}
-        </p>
-        <p>support@vitalflohealth.com </p>
-        <p>or</p>
-        <p>+1 844 542 3656</p>
-        <p>We will promptly respond.</p>
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className='text-9xl font-bold dark:text-zinc-50'>404</h1>
+        <h2 className='text-4xl font-medium dark:text-zinc-50'>
+          Page Not Found
+        </h2>
+        <a
+          href='/'
+          className='mt-4 flex items-center justify-center text-lg font-medium text-gray-600 hover:text-gray-700 dark:text-zinc-50 dark:hover:text-zinc-50'
+        >
+          <button className='bg-gray-900 px-3 py-2 text-xs text-zinc-50 sm:px-4 sm:text-sm md:text-lg'>
+            Home Page
+          </button>
+        </a>
       </div>
     </div>
   )
