5ceadcb6057aa7d552b12574c722fe911978a124
Jason O'Dell <jason@vitalflohealth.com>
2023-04-13 00:03:24 -0400

    Updating global error page

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -9,12 +9,9 @@ export default function GlobalError({
 }) {
   return (
     <html>
-      <head></head>
       <body>
         <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>
-          Try again
-        </button>
+        <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
   )
