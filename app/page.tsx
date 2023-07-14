a5210069992ffd31f24c2548b5da1992325df290
Jason O'Dell <jason@vitalflohealth.com>
2023-07-13 23:56:00 -0400

    Adding new logo pic, drone lineup pic, and tagline and reordering some images

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import booneAndSon from '../public/images/boone_and_son.jpg'
 import booneInField from '../public/images/boone_in_field.jpg'
 import booneUASLogo from '../public/images/boone_uas_logo.jpg'
 import booneWithDrone from '../public/images/boone_with_drone.jpg'
+import droneLineup from '../public/images/drone_lineup.png'
 import faaSeal from '../public/images/faa_seal.jpg'
 import topOperator from '../public/images/top_operator.png'
 import trainingPlatformsDji from '../public/images/training_platforms_dji.png'
@@ -23,7 +24,7 @@ export default async function Index() {
       <main id='home'>
         <section id='hero'>
           <div className='mx-auto grid max-w-7xl grid-cols-2 px-4 py-16 sm:px-6 lg:px-16'>
-            <div className='col-span-1 flex flex-col items-start justify-start px-2'>
+            <div className='col-span-1 flex flex-col items-start justify-center px-2'>
               <div className='w-1/2'>
                 <Image
                   src={booneUASLogo}
@@ -32,10 +33,18 @@ export default async function Index() {
                   height={400}
                 />
               </div>
-              <h2 className='mt-6 text-xl dark:text-zinc-200 sm:text-2xl md:mt-8 md:text-4xl'>
+              <h2 className='mt-2 md:mt-4 lg:mt-6 ml-1 xs:ml-[0.5rem] sm:ml-3 md:ml-4 lg:ml-5 xl:ml-6 text-lg xs:text-xl dark:text-zinc-200 sm:text-2xl md:text-3xl'>
                 Drone Service Provider
               </h2>
-              <h3 className='mt-2 text-base italic dark:text-zinc-200 md:mt-4 md:text-2xl font-normal'>Unmanned Aerial Systems</h3>
+              <h3 className='mt-2 ml-1 xs:ml-[0.5rem] sm:ml-3 md:ml-4 lg:ml-5 xl:ml-6 text-sm xs:text-base italic dark:text-zinc-200 md:mt-4 md:text-2xl font-normal'>Unmanned Aerial Systems</h3>
+              <div className='mt-4 md:mt-6 lg:mt-8 mx-1 xs:mx-[0.5rem] sm:mx-3 md:mx-4 lg:mx-5 xl:mx-6'>
+                <Image
+                  src={droneLineup}
+                  alt='Drone Lineup'
+                  width={400}
+                  height={100}
+                />
+              </div>
               {/* <h4 className='mt-6 md:text-xl'>Need a drone?</h4>
               <a href='#contact'>
                 <button className='mt-2 bg-gray-900 px-3 py-2 text-xs text-zinc-50 hover:bg-gray-700 focus:outline-gray-700 sm:px-4 sm:text-sm md:text-lg'>
@@ -46,15 +55,18 @@ export default async function Index() {
             <div className='col-span-1 m-auto h-full w-full px-2'>
               <Slideshow />
             </div>
-            <div className="col-span-2 flex justify-center items-center mt-12 md:mt-24">
-              <h4 className='md:text-xl mr-2 font-bold'>Need a drone?</h4>
-              <a className='group flex justify-center items-center' href='#contact'>
-                {/* <p className='text-xs sm:text-sm md:text-xl underline underline-offset-[6px] hover:underline-offset-4 decoration-gray-500 hover:decoration-gray-700 transition-all duration-100 ease-linear'>Get started today.</p> */}
-                <p className='md:text-xl underline underline-offset-[6px] decoration-gray-700 hover:no-underline bg-[length:100%_2px] bg-left-bottom hover:bg-left-bottom bg-gradient-to-r hover:bg-gradient-to-r hover:from-blue-500 hover:to-blue-900 bg-no-repeat group-hover:bg-[length:100%_2px]'>Get started today.</p>
-                {/* <button className='bg-gray-900 px-3 py-2 text-xs text-zinc-50 hover:bg-gray-700 focus:outline-gray-700 sm:px-4 sm:text-sm md:text-lg'>
+            <div className="col-span-2 flex flex-col justify-center items-center mt-12 md:mt-24">
+              <h3 className='tracking-wide text-lg xs:text-xl sm:text-2xl md:text-3xl text-center'>We fly. We film. We deliver.</h3>
+              <div className='mt-12 flex items-center justify-center'>
+                <h4 className='md:text-xl mr-2 font-bold'>Need a drone?</h4>
+                <a className='group flex justify-center items-center' href='#contact'>
+                  {/* <p className='text-xs sm:text-sm md:text-xl underline underline-offset-[6px] hover:underline-offset-4 decoration-gray-500 hover:decoration-gray-700 transition-all duration-100 ease-linear'>Get started today.</p> */}
+                  <p className='md:text-xl underline underline-offset-[6px] decoration-gray-700 hover:no-underline bg-[length:100%_2px] bg-left-bottom hover:bg-left-bottom bg-gradient-to-r hover:bg-gradient-to-r hover:from-blue-500 hover:to-blue-900 bg-no-repeat group-hover:bg-[length:100%_2px]'>Get started today.</p>
+                  {/* <button className='bg-gray-900 px-3 py-2 text-xs text-zinc-50 hover:bg-gray-700 focus:outline-gray-700 sm:px-4 sm:text-sm md:text-lg'>
                   Get Started Today
                 </button> */}
-              </a>
+                </a>
+              </div>
             </div>
           </div>
         </section>
@@ -105,17 +117,17 @@ export default async function Index() {
                   <p className='dark:text-zinc-200'>UAS Consulting</p>
                 </div>
                 <div className="grid grid-cols-2 w-32 grid-rows-2 gap-2 mt-2">
-                  <div className='row-span-1 col-span-1 flex justify-center items-center'>
-                    <Image src={trainingPlatformSkydio} alt='Skydio Logo' width={64} />
-                  </div>
-                  <div className='row-span-1 col-span-1 flex justify-center items-center'>
-                    <Image src={trainingPlatformsDji} alt='DJI Logo' width={40} />
-                  </div>
                   <div className="row-span-1 col-span-1 flex justify-center items-center bg-transparent">
                     <Image src={trainingPlatformsParrot} alt='Parrot Logo' width={64} />
                   </div>
                   <div className='row-span-1 col-span-1 flex justify-center items-center'>
                     <Image src={trainingPlatformsWingtra} alt='Wingtra Logo' width={64} />
+                  </div>
+                  <div className='row-span-1 col-span-1 flex justify-center items-center'>
+                    <Image src={trainingPlatformsDji} alt='DJI Logo' width={40} />
+                  </div>
+                  <div className='row-span-1 col-span-1 flex justify-center items-center'>
+                    <Image src={trainingPlatformSkydio} alt='Skydio Logo' width={64} />
                   </div>
                 </div>
               </div>
